d56a60602cfd576662016d53ff80bd704d6d7970
joyunju <whdbswn1113@naver.com>
2022-04-18 03:26:42 +0900

    1

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -4,5 +4,18 @@ package com.javaex.practice;
 public class Ex01 {
 	public static void main(String[] args) {
 		
+//		true는 예약어라서 사용 불가
+//		int true;
+//		특수문자 사용 불가 
+//		int #_of_workers;
+//		예약어는 식별자로 사용 불가 
+//		int countOfLettersInString;
+//		숫자로 시작 불가 
+//		int 1stLevel1;
+//		특수문자 사용 불가 
+//		int person#;
+//		$와 _는 예외로 사용 가능 
+//		int _person;
+		
 	}
 }
