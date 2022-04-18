fc997a7b75af2e84c52e2c43e0dc31a294c7aac7
joyunju <whdbswn1113@naver.com>
2022-04-18 16:08:21 +0900

    double형의 변수 선언 -> 연산

---
+++ src/com/javaex/practice/Ex05.java
@@ -0,0 +1,20 @@
+//다음의 요구사항을 만족하는 코드를 작성하세요.
+
+//double 형의 speed, time, distance 변수를 선언한다.
+//speed 변수에 90.0을 저장한다.
+//time 변수에 60.0을 저장한다.
+//speed와 time을 곱하고 그 결과를 distance에 저장한다.
+//distance를 콘솔에 출력한다.
+
+package com.javaex.practice;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		
+		double speed = 90.0;
+		double time = 60.0;
+		double distance = speed * time;
+		
+		System.out.println(distance);
+	}
+}
