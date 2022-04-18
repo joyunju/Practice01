8048c9f9b784aed831484fda792b0f57f30ee21a
joyunju <whdbswn1113@naver.com>
2022-04-18 11:24:50 +0900

    연산

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,13 @@
+//
+package com.javaex.practice;
+
+public class Ex02 {
+	public static void main(String[] args) {
+
+		System.out.println(12 / 5 - 3); // 결과값 : -1
+		//12 나누기 5 = 2.4 -3 --> 기본 연산이 정수라  2-3 = -1
+
+		System.out.println(5 + 19 % 3); // 결과값 : 6
+		// 19에서 3을 나눈 후 몫 = 1 --> 1+5 = 6
+	}
+}
