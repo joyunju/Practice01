b180d95e59af7134a4120b06717f402340fd91ee
joyunju <whdbswn1113@naver.com>
2022-04-25 15:03:21 +0900

    변수 값 대입

---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,14 @@
+//Ex10 다음의 코드를 컴파일 할 때 오류가 발생한다. 오류의 원인은?
+package com.javaex.practice;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		
+		//int x; --> 오류 이유 : 변수 x에 들어가 있는 값이 없음.
+		
+		//수정 
+		int x = 10;
+		System.out.println(x);
+
+	}
+}
